ac67714fa35d7dad9539f9dedd4f65288a890e2d
Fernando M. Oliveira <fmoraes@mercadocar.com.br>
2022-04-15 13:27:56 -0300

    Atualizada para a imagem mcr.microsoft.com/mssql/server:2019-latest

--- infra/config.sh
+++ infra/config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #command line sqlcmd execute scripts
 export SQL_CMD=/opt/mssql-tools/bin/sqlcmd
 
@@ -12,10 +14,10 @@ QUERY_RESULT_FILE=$PATH_LOG/query-result.txt
 SCRIPTS_FOLDER=./scripts
 
 # sqlserver configurations
-export HOST_NAME_SQL_SERVER=localhost 
+export HOST_NAME_SQL_SERVER=localhost
 export USER_SA=sa
-export SA_PASSWORD=Admin@123 
-export DATABASE_NAME=test_db 
-export USERNAME_APPLICATION=application_user 
-export PASSWORD_APPLICATION=Pass_word_123 
+export SA_PASSWORD=Admin@123
+export DATABASE_NAME=test_db
+export USERNAME_APPLICATION=application_user
+export PASSWORD_APPLICATION=Pass_word_123
 export CONTAINER_NAME=sqlserver-qa
